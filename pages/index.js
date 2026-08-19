c1b1e7fc6be96bd44f97415eab2fe11c104b5486
awdawfawd789-alt <309603125+awdawfawd789-alt@users.noreply.github.com>
2026-08-19 08:56:53 -0600

    Add index page to prevent 404 on root

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontFamily: 'monospace', backgroundColor: '#000', color: '#0f0' }}>
+      <h1>API is Online</h1>
+    </div>
+  )
+}
